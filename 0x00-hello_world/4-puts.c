9a1d321db7860c947711a54343d42f8c38fb56b0
otim-emma <otimemma270@gmail.com>
2023-08-14 12:49:58 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 
+
+
 /**
- * main - Entry point
+ * main - prints "Programming is like building a multilingual puzzle, followed by a newline."
  *
- * Return: Always 0 (Success)
+ * return 0.
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle\n");
-return (0);
+	
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
